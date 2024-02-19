bf8009500cccbd1f5da1f51b7011ccc364c4ca9e
Germano Silva <silvadeveloper2024@gmail.com>
2024-02-19 06:31:28 -0300

    chore: criação do cabeçalho HeaderPage

---
+++ src/app/header/page.tsx
@@ -0,0 +1,9 @@
+const HeaderPage = () => {
+    return ( 
+        <div className="w-full flex ">
+            <h1 className="font-Bebas_Neue">Silva<p>Dev</p></h1>
+        </div>
+     );
+}
+ 
+export default HeaderPage;
