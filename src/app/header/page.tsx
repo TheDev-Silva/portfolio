998f476545de36e5c62cadf61fe56bc63f35163f
Germano Silva <silvadeveloper2024@gmail.com>
2024-02-20 06:30:20 -0300

    feat: melhorias no header

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -4,36 +4,36 @@ import Link from "next/link";
 
 const HeaderPage = () => {
   return (
-    <div className="w-full flex items-center justify-between p-10 bg-gradient-to-br from-slate-300 to-black">
-      <div className="flex items-center gap-4">
-        <div className="min-w-14 flex items-end bg-orange-500 p-2 rounded-xl hover:rounded-xl hover:duration-700 hover:rotate-12 overflow-hidden">
-          <Image
-            src="/banner-bolo-confeitados.png"
-            alt="foto"
-            width={500}
-            height={500}
-            className="w-14 hover:scale-150 hover:duration-700 transition-transform hover:rotate-12 "
-          />
+    <div className="w-full h-[150px] px-8 flex items-center  justify-between hover:shadow-lg duration-500 hover:h-[160px]">
+        <div className="w-[50%] flex items-center gap-4">
+          <div className="min-w-14 flex items-end bg-orange-500 p-2 rounded-xl hover:rounded-xl hover:duration-700 hover:rotate-12 overflow-hidden">
+            <Image
+              src="/banner-bolo-confeitados.png"
+              alt="foto"
+              width={500}
+              height={500}
+              className="w-14 hover:scale-150 hover:duration-700 transition-transform hover:rotate-12"
+            />
+          </div>
+          <h1 className="font-Bebas_Neue text-4xl text-white tracking-widest">silva developer</h1>
         </div>
-        <h1 className="font-Bebas_Neue text-4xl">silva developer</h1>
+        <div className="w-full flex items-center justify-between">
+          <div className="w-full flex gap-4 ">
+            <Link href="/" className="min-w-24 max-w-32 flex items-center justify-center font-normal text-md hover:text-white hover:duration-500 uppercase">Home
+            </Link>
+            <Link href="/about" className="min-w-24 max-w-32 flex items-center justify-center font-normal text-md hover:text-white hover:duration-500 uppercase">About 
+            </Link>
+            <Link href="/portifolio" className="min-w-24 max-w-32 flex items-center justify-center font-normal text-md hover:text-white hover:duration-500 uppercase">Portifólio 
+            </Link>
+            <Link href="/contact" className="min-w-24 max-w-32 flex items-center justify-center font-normal text-md hover:text-white hover:duration-500 uppercase after:w-26 ">contatos 
+            </Link>
+
+          </div>
+          <div className="w-full flex justify-end">
+            <Button className="bg-orange-500 text-black hover:text-white font-semibold w-24 items uppercase">Let's Go!</Button>
+          </div>
+        </div>
       </div>
-      <div className="flex gap-4">
-        <Link href="/">
-          <Button className="min-w-24 max-w-32 rounded-lg">Home</Button>
-        </Link>
-        <Link href="/about">
-          <Button className="min-w-24 max-w-32 rounded-lg">About</Button>
-        </Link>
-        <Link href="/portifolio">
-          <Button className="min-w-24 max-w-32 rounded-lg">Portifólio</Button>
-        </Link>
-        <Link href="/contact">
-          <Button className="min-w-24 max-w-32 rounded-lg">contatos</Button>
-        </Link>
-        
-      </div>
-      <Button className="bg-orange-500">Let's Go!</Button>
-    </div>
   );
 }
 
