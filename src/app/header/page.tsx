ca633fe4a7b86c6eb8448bc0cde0b5bb36fdcd54
Germano Silva <silvadeveloper2024@gmail.com>
2024-02-19 22:37:34 -0300

    feat: atualizando header na page Home

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 const HeaderPage = () => {
   return (
-    <div className="w-full flex items-center justify-between p-10">
+    <div className="w-full flex items-center justify-between p-10 bg-gradient-to-br from-slate-300 to-black">
       <div className="flex items-center gap-4">
         <div className="min-w-14 flex items-end bg-orange-500 p-2 rounded-xl hover:rounded-xl hover:duration-700 hover:rotate-12 overflow-hidden">
           <Image
@@ -15,10 +17,22 @@ const HeaderPage = () => {
         </div>
         <h1 className="font-Bebas_Neue text-4xl">silva developer</h1>
       </div>
-      <div>
+      <div className="flex gap-4">
+        <Link href="/">
+          <Button className="min-w-24 max-w-32 rounded-lg">Home</Button>
+        </Link>
+        <Link href="/about">
+          <Button className="min-w-24 max-w-32 rounded-lg">About</Button>
+        </Link>
+        <Link href="/portifolio">
+          <Button className="min-w-24 max-w-32 rounded-lg">Portifólio</Button>
+        </Link>
+        <Link href="/contact">
+          <Button className="min-w-24 max-w-32 rounded-lg">contatos</Button>
+        </Link>
         
       </div>
-
+      <Button className="bg-orange-500">Let's Go!</Button>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,46 @@
-import HeaderPage from "./header/page"
-
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <HeaderPage/>
-      <h1>Bem vindo ao meu Portifólio Profissional</h1>
+    <div className="w-full h-screen flex  bg-gradient-to-br from-slate-500 to-gray-200">
+      {/* header */}
+      <div className="w-full h-[150px] px-8 grid grid-cols-2 items-center  justify-between">
+        <div className="w-full flex items-center gap-4">
+          <div className="min-w-14 flex items-end bg-orange-500 p-2 rounded-xl hover:rounded-xl hover:duration-700 hover:rotate-12 overflow-hidden">
+            <Image
+              src="/banner-bolo-confeitados.png"
+              alt="foto"
+              width={500}
+              height={500}
+              className="w-14 hover:scale-150 hover:duration-700 transition-transform hover:rotate-12"
+            />
+          </div>
+          <h1 className="font-Bebas_Neue text-4xl text-white tracking-widest">silva developer</h1>
+        </div>
+        <div className="w-full flex items-center justify-between ">
+          <div className="w-full flex gap-4 ">
+            <Link href="/">
+              <Button className="min-w-24 max-w-32 rounded-lg hover:bg-gray-50 hover:text-black uppercase">Home</Button>
+            </Link>
+            <Link href="/about">
+              <Button className="min-w-24 max-w-32 rounded-lg hover:bg-gray-50 hover:text-black uppercase">About</Button>
+            </Link>
+            <Link href="/portifolio">
+              <Button className="min-w-24 max-w-32 rounded-lg hover:bg-gray-50 hover:text-black uppercase">Portifólio</Button>
+            </Link>
+            <Link href="/contact">
+              <Button className="min-w-24 max-w-32 rounded-lg hover:bg-gray-50 hover:text-black uppercase">contatos</Button>
+            </Link>
+
+          </div>
+          <div className="w-full flex justify-end">
+            <Button className="bg-orange-500 text-black hover:text-white font-semibold w-24 items uppercase">Let's Go!</Button>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
