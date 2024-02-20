f361c890d4e942bd5d70745e91282d4d4810e8f1
Germano Silva <silvadeveloper2024@gmail.com>
2024-02-20 20:56:12 -0300

    feat: atualizando pagina

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -6,7 +6,7 @@ const HeaderPage = () => {
   return (
     <div className="w-full h-[150px] px-8 flex items-center  justify-between hover:shadow-lg duration-500 hover:h-[160px]">
         <div className="w-[50%] flex items-center gap-4">
-          <div className="min-w-14 flex items-end bg-orange-500 p-2 rounded-xl hover:rounded-xl hover:duration-700 hover:rotate-12 overflow-hidden">
+          <div className="min-w-14 flex items-center bg-[#ff8800] p-2 rounded-xl hover:rounded-xl hover:duration-700 hover:rotate-12 overflow-hidden justify-center shadow-md">
             <Image
               src="/banner-bolo-confeitados.png"
               alt="foto"
@@ -15,22 +15,22 @@ const HeaderPage = () => {
               className="w-14 hover:scale-150 hover:duration-700 transition-transform hover:rotate-12"
             />
           </div>
-          <h1 className="font-Bebas_Neue text-4xl text-white tracking-widest">silva developer</h1>
+          <h1 className="font-Roseberry text-2xl text-white tracking-widest">silva developer</h1>
         </div>
         <div className="w-full flex items-center justify-between">
           <div className="w-full flex gap-4 ">
-            <Link href="/" className="min-w-24 max-w-32 flex items-center justify-center font-normal text-md hover:text-white hover:duration-500 uppercase">Home
+            <Link href="/" className="min-w-24 max-w-32 flex items-center justify-center text-[16px] hover:text-white hover:scale-100 hover:duration-500 uppercase">Home
             </Link>
-            <Link href="/about" className="min-w-24 max-w-32 flex items-center justify-center font-normal text-md hover:text-white hover:duration-500 uppercase">About 
+            <Link href="/about" className="min-w-24 max-w-32 flex items-center justify-center text-[16px] hover:text-white hover:duration-500 uppercase">About 
             </Link>
-            <Link href="/portifolio" className="min-w-24 max-w-32 flex items-center justify-center font-normal text-md hover:text-white hover:duration-500 uppercase">Portifólio 
+            <Link href="/portifolio" className="min-w-24 max-w-32 flex items-center justify-center text-[16px] hover:text-white hover:duration-500 uppercase">Portifólio 
             </Link>
-            <Link href="/contact" className="min-w-24 max-w-32 flex items-center justify-center font-normal text-md hover:text-white hover:duration-500 uppercase after:w-26 ">contatos 
+            <Link href="/contact" className="min-w-24 max-w-32 flex items-center justify-center text-[16px] hover:text-white hover:duration-500 uppercase after:w-26 ">contatos 
             </Link>
 
           </div>
           <div className="w-full flex justify-end">
-            <Button className="bg-orange-500 text-black hover:text-white font-semibold w-24 items uppercase">Let's Go!</Button>
+            <Button className="bg-[#ff8800] text-black hover:text-white font-semibold w-24 items uppercase">Let's Go!</Button>
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,12 @@ export default function Home() {
     <div className="w-full h-screen flex flex-col bg-gradient-to-br from-[#56B0F8] to-[#24476A]">
       {/* header */}
       <HeaderPage />
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 p-8">
         <div>
-          <h1 className="uppercase text-6xl">germano silva,</h1>
+          <h1 className="text-6xl font-Geometr415_Blk_BT">Germano Silva,</h1>
+          <p className="text-[20px] pt-6 text-white">
+            Desenvolvedor Front-End, Residindo em Curitiba-PR
+          </p>
         </div>
 
       </div>
